265130a4ca876484492c96c0a75854a0c3ac013f
KinNguyen <nguyenhuukien1994@gmail.com>
2016-11-04 16:09:48 +0700

    delete

--- TCKH/src/main/java/net/codejava/spring/dao/CategoryDAO.java
+++
@@ -1,9 +0,0 @@
-package net.codejava.spring.dao;
-import java.util.List;
-
-import net.codejava.spring.model.Category;
-
-public interface CategoryDAO {
-	public Category get(int id);
-	public List<Category> list();
-}
